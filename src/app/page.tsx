7ef20d88309d2f2dccfd286edf93f166957c356c
Jay Do <jay@cal-se.com>
2023-06-28 15:26:25 +0700

    build: :rocket: To the moon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,101 @@
-import Image from 'next/image'
+import Image from 'next/image';
+import Link from 'next/link';
+import { FaTwitter, FaTelegram } from 'react-icons/fa';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
+    <main className='bg-glow-pink w-full flex flex-col'>
+      <div className='fixed top-6 left-6 flex w-fit'>
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+          width={109}
+          height={113}
+          src={'/logo.png'}
+          alt='Logo of 52 Elon'
+        />
+        <Image
+          width={303}
+          height={113}
+          src={'/elonmusk-text.png'}
+          alt='Name of 52 Elon'
         />
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className='h-screen w-full max-w-[1600px] max-h-[1100px] mx-auto flex relative'>
+        <Image
+          className='absolute bottom-10 -left-12 flex w-3/4 h-auto'
+          width={1900}
+          height={1000}
+          src={'/elonwithcake.png'}
+          alt='52 Elon with cake'
+        />
+        <div className='absolute flex flex-col justify-start items-center right-12 top-24'>
+          <Image
+            className='flex'
+            width={600}
+            height={600}
+            src={'/hpbd52.png'}
+            alt='Happy birhtday 52 Elon'
+          />
+          <div className='flex mx-auto'>
+            <Link
+              className='rounded-full bg-[#3B90CD] px-5 py-2 text-white font-bold text-xl'
+              href={
+                'https://www.pinksale.finance/launchpad/0x2EFac431a2E98020f3Ef5ea75C9Fb544D2d35Ba4?chain=ETH'
+              }
+              target='_blank'
+            >
+              CHART
+            </Link>
+            <Link
+              className='rounded-full bg-[#B54D68] ml-3 px-5 py-2 text-white font-bold text-xl'
+              href={
+                'https://www.pinksale.finance/launchpad/0x2EFac431a2E98020f3Ef5ea75C9Fb544D2d35Ba4?chain=ETH'
+              }
+              target='_blank'
+            >
+              PRESALE
+            </Link>
+            <Link
+              className='rounded-full bg-orange-500 ml-3 px-5 py-2 text-white font-bold text-xl'
+              href={
+                'https://blocksafu.com/audit/0xE636043F8f9E5407f8E70e0D4E71c7F0a0AcD5F8'
+              }
+              target='_blank'
+            >
+              AUDIT
+            </Link>
+          </div>
+        </div>
       </div>
+      <footer className='w-full fixed bottom-0 bg-[#FA8198] flex justify-center items-center py-2'>
+        <Link
+          className='rounded-sm bg-[#F95192] w-12 h-12 flex justify-center items-center mr-2'
+          href={
+            'https://www.pinksale.finance/launchpad/0x2EFac431a2E98020f3Ef5ea75C9Fb544D2d35Ba4?chain=ETH'
+          }
+          target='_blank'
+        >
+          <Image
+            width={28}
+            height={28}
+            src={'/pinksale.png'}
+            alt='PinkSale of 52 Elon'
+          />
+        </Link>
+        <Link
+          className='rounded-sm bg-[#34AADF] w-12 h-12 flex justify-center items-center mr-2'
+          href={'https://t.me/happy52thelonportal'}
+          target='_blank'
+        >
+          <FaTelegram size={26} color={'white'} />
+        </Link>
+        <Link
+          className='rounded-sm bg-[#33CCFF] w-12 h-12 flex justify-center items-center'
+          href={'https://twitter.com/52thElon'}
+          target='_blank'
+        >
+          <FaTwitter size={24} color={'white'} />
+        </Link>
+      </footer>
     </main>
-  )
+  );
 }
